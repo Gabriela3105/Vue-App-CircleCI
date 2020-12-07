85d38fd0d53b8379b9cfa793cc0a05984e388e86
Gabriela <gabytachavez_93@hotmail.com>
2020-12-07 14:29:41 +0300

    changes in home.vue 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="banner">
       <div class="container">
         <h1 class="logo-font">conduit Gabriela Chavez</h1>
-        <p>A place to share your knowledge 2.</p>
+        <p>A place to share your knowledge 333.</p>
       </div>
     </div>
     <div class="container page">
